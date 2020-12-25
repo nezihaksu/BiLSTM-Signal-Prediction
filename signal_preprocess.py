14421dfd34faea0cc727cb12c8e5f824300b6d63
nezih <nezih-aksu@hotmail.com>
2020-12-25 20:31:10 +0300

    Indexing dataframes according to time.

--- signal_preprocess.py
+++ signal_preprocess.py
@@ -7,148 +7,167 @@ import itertools
 label = pd.read_csv(r"C:\Users\nezih\Desktop\hw3\sensor-27minutes.csv")
 data_input = label.iloc[:120_000, 1].values
 
+raw_value = pd.read_csv(r"C:\Users\nezih\Desktop\hw3\sensor-27minutes.csv")
+targets = pd.read_excel(r"C:\Users\nezih\Desktop\hw3\label-27minutes.xlsx")
 
-def butter_bandpass(lowcut, highcut, fs, order):
-    nyq = 0.5 * fs
-    low = lowcut / nyq
-    high = highcut / nyq
-    b, a = butter(order, [low, high], btype='band')
-    return b, a
+train_start_hour = 12
+train_start_minute = 19
+train_end_hour = 12
+train_end_minute = 39
 
-
-def butter_bandpass_filter(data, lowcut, highcut, fs, order):
-    b, a = butter_bandpass(lowcut, highcut, fs, order=order)
-    y = lfilter(b, a, data)
-    return y
-
-
-def run_butter_filter(x, lowcut, highcut, fs, order):
-    b, a = butter_bandpass(lowcut, highcut, fs, order=order)
-    y = butter_bandpass_filter(x, lowcut, highcut, fs, order)
-    return y
+test_start_hour = 12
+test_start_minute = 39
+test_end_hour = 12
+test_end_minute = 46
 
 
-def moving_avarage_filter(x, order):
-    b = (1 / order) * np.ones((order, 1))
-    y = lfilter(b.flatten(), 1.0, x.flatten())
-    return y
-def segmentation_fn(temp,seg_sample_num,seg_nember):
-    y = []
-    for x in range(seg_nember):
-        thr = np.var(temp[x, :], ddof=1) * 0.7
-        indices = [0, seg_sample_num - 2]
-        a = 0
-        b = 1
-        while indices[a] != seg_sample_num - 2:
-            if np.var(temp[x, indices[a]:indices[b] + 1], ddof=1) > thr:
-                c = math.floor((indices[b] + indices[a]) / 2)
-                if c in indices:
+class signal_processing:
+    def __init__(self, value,id_number):
+        self.value = value
+        self.id_number = id_number
+
+    def butter_bandpass(self,lowcut, highcut, fs, order):
+        nyq = 0.5 * fs
+        low = lowcut / nyq
+        high = highcut / nyq
+        b, a = butter(order, [low, high], btype='band')
+        return b, a
+
+    def butter_bandpass_filter(self,data, lowcut, highcut, fs, order):
+        b, a = self.butter_bandpass(lowcut, highcut, fs, order=order)
+        y = lfilter(b, a, data)
+        return y
+
+    def run_butter_filter(self,x, lowcut, highcut, fs, order):
+        b, a = self.butter_bandpass(lowcut, highcut, fs, order=order)
+        y = self.butter_bandpass_filter(x, lowcut, highcut, fs, order)
+        return y
+
+    def moving_avarage_filter(self,x, order):
+        b = (1 / order) * np.ones((order, 1))
+        y = lfilter(b.flatten(), 1.0, x.flatten())
+        return y
+
+    def segmentation_fn(self,temp, seg_sample_num, seg_nember, bin_length):
+        y = []
+        for x in range(seg_nember):
+            thr = np.var(temp[x, :], ddof=1) * 0.7
+            indices = [0, seg_sample_num - 2]
+            a = 0
+            b = 1
+            while indices[a] != seg_sample_num - 2:
+                if np.var(temp[x, indices[a]:indices[b] + 1], ddof=1) > thr:
+                    c = math.floor((indices[b] + indices[a]) / 2)
+                    if c in indices:
+                        a = a + 1
+                        b = b + 1
+                    else:
+                        indices.append(c)
+                        indices.sort()
+                else:
                     a = a + 1
                     b = b + 1
-                else:
-                    indices.append(c)
-                    indices.sort()
+
+            if len(indices) > bin_length:
+                var_bin = []
+                for j in range(len(indices) - 1):
+                    var_bin.append([np.var(temp[x, indices[j]:indices[j + 1] + 1], ddof=1), j])
+                var_bin.sort()
+                sort_index = []
+                for x in var_bin:
+                    sort_index.append(x[1])
+                index_flip = np.flip(sort_index)
+                new_idx = index_flip[0:bin_length - 1]
+                new_idx1 = []
+                for w in range(len(new_idx)):
+                    new_idx1.append(indices[new_idx[w]])
+                new_idx1.sort()
+                y.append(new_idx1)
             else:
-                a = a + 1
-                b = b + 1
+                l = math.floor(seg_sample_num / 15)
+                d = []
+                for i in range(15):
+                    d.append([i * l])
+                y.append(d)
+        return y
 
-        if len(indices) > bin_length:
-            var_bin = []
-            for j in range(len(indices) - 1):
-                var_bin.append([np.var(temp[x, indices[j]:indices[j + 1] + 1], ddof=1), j])
-            var_bin.sort()
-            sort_index = []
-            for x in var_bin:
-                sort_index.append(x[1])
-            index_flip = np.flip(sort_index)
-            new_idx = index_flip[0:bin_length]
-            new_idx1 = []
-            for w in range(len(new_idx)):
-                new_idx1.append(indices[new_idx[w]])
-            new_idx1.sort()
-            y.append(new_idx1)
-    return y
+    def signalProcessingFun(self):
+        ##### butherworth bandpass filtering###
 
-##### butherworth bandpass filtering###
+        fs = 100.0
+        lowcut = 1.0
+        highcut = 9.0
+        order = 5
+        dataOut_filter = self.run_butter_filter(self.value, lowcut, highcut, fs, order)
 
-fs = 100.0
-lowcut = 1.0
-highcut = 8.0
-order = 4
-dataOut_filter = run_butter_filter(data_input, lowcut, highcut, fs, order)
+        ##### Reduction Of abnormality in signal by superposition of moving averages and normalization
 
-##### Reduction Of abnormality in signal by superposition of moving averages and normalization
+        data_length = len(dataOut_filter)
+        segmentation_length = 100
+        filter_order = 8
+        segments_numbers = math.floor(data_length / segmentation_length)
+        temp = np.subtract(dataOut_filter, np.mean(dataOut_filter))
+        temp1 = np.reshape(temp[0:segments_numbers * segmentation_length], (segments_numbers, segmentation_length))
 
-filter_order = 8
-out_moving_filter = moving_avarage_filter(dataOut_filter, filter_order)
-out_mf1 = dataOut_filter[0:len(dataOut_filter)-3] - out_moving_filter[3:len(dataOut_filter)]
+        out_moving_filter = np.zeros((segments_numbers, segmentation_length))
+        temp11 = np.zeros((segments_numbers, segmentation_length))
+        for i in range(1, segments_numbers):
+            out_moving_filter[i, :] = self.moving_avarage_filter(temp1[i, :], filter_order)
 
-out_mf11 = np.subtract(out_mf1,np.mean(out_mf1))
-out_mf111 = np.true_divide(out_mf11,np.sqrt(np.var(out_mf11,ddof=1)))
+        var_segmentation = []
+        for element in out_moving_filter:
+            var_segmentation.append(np.var(element, ddof=1) / 10)
 
-data_length = len(dataOut_filter)
-segmentation_length = 100
-filter_order = 8
-segments_numbers = math.floor(data_length / segmentation_length)
-temp = np.subtract(dataOut_filter, np.mean(dataOut_filter))
-temp1 = np.reshape(temp[0:segments_numbers * segmentation_length], (segments_numbers,segmentation_length))
+        for i in range(1, segments_numbers):
+            temp11[i, :] = temp1[i, :] / var_segmentation[i]
 
-out_moving_filter = np.zeros((segments_numbers,segmentation_length))
-temp11 = np.zeros((segments_numbers,segmentation_length))
-for i in range(1, segments_numbers):
+        dataOut = temp11.flatten()
+        dataOut_filter = self.run_butter_filter(dataOut, lowcut, highcut, fs, order)
+        dataOut = np.subtract(dataOut_filter, np.mean(dataOut_filter))
+        dataOut = np.true_divide(dataOut, np.sqrt(np.var(dataOut, ddof=1)))
 
+        ##### adaptive segmentations
 
-     out_moving_filter[i, :] = moving_avarage_filter(temp1[i,:], filter_order)
+        bins_num = self.id_number
+        bin_length = 16
+        seg_nember = math.floor(bins_num / bin_length)
+        seg_sample_num = math.floor(len(dataOut) / seg_nember)
+        temp = np.reshape(dataOut[0:seg_nember * seg_sample_num], (seg_nember, seg_sample_num))
+        bin_arrange = []
+        bin_arrange = self.segmentation_fn(temp, seg_sample_num, seg_nember, bin_length)
 
-var_segmentation = []
-for element in out_moving_filter:
-    var_segmentation.append(np.var(element,ddof=1)/10)
+        id_list = []
+        id_samples = []
+        num = 0
+        for x in range(seg_nember):
+            temp1 = bin_arrange[x]
+            temp11 = temp[x, :]
+            r = 0
+            for y in range(bin_length):
+                if y == bin_length - 1:
+                    id_list.append([num] * (seg_sample_num - r))
+                    id_samples.append((temp11[r:seg_sample_num]))
+                else:
+                    id_list.append([num] * (temp1[y] - r))
+                    id_samples.append((temp11[r:temp1[y]]))
+                    r = temp1[y]
+                    num = num + 1
 
-for i in range(1,segments_numbers):
-    temp11[i,:] = temp1[i,:]/var_segmentation[i]
+        return id_list,id_samples
 
-dataOut = temp11.flatten()
+#Indexing dataframes according to hour and minutes.
+def train_test_index(start_hour,start_min,end_hour,end_min):
+    start_time = "{}:{}".format(start_hour,start_min)
+    end_time = "{}:{}".format(end_hour,end_min)
+    
+    raw_value_start_index = raw_value[raw_value.Time.str.contains(start_time)].iloc[0,:].name
+    raw_value_end_index = raw_value[raw_value.Time.str.contains(end_time)].iloc[0,:].name
+    
+    raw_scg_value = raw_value.iloc[raw_value_start_index:raw_value_end_index, 1].values
+    
+    targets_start_index = targets[targets["Raw Time"].astype(str).str.contains(start_time)].iloc[0,:].name  
+    targets_end_index = targets[targets["Raw Time"].astype(str).str.contains(end_time)].iloc[0,:].name
+    
+    id_number = targets_end_index - targets_start_index
 
-fs = 100.0
-lowcut = 1.0
-highcut = 9.0
-order = 4
-dataOut_filter = run_butter_filter(dataOut, lowcut, highcut, fs, order)
-
-dataOut = np.subtract(dataOut_filter,np.mean(dataOut_filter))
-dataOut = np.true_divide(dataOut,np.sqrt(np.var(dataOut,ddof=1)))
-
-
-##### adaptive segmentations
-
-bins_num = 1200#len()
-bin_length = 16
-seg_nember = math.floor(bins_num / bin_length)
-seg_sample_num = math.floor(len(dataOut)/seg_nember)
-temp = np.reshape(dataOut[0:seg_nember*seg_sample_num],(seg_nember,seg_sample_num))
-bin_arrange = []
-bin_arrange = segmentation_fn(temp,seg_sample_num,seg_nember)
-
-id_list = []
-id_samples = []
-num = 0
-for x in range(seg_nember):
-    temp1 = bin_arrange[x]
-    temp11 = temp[x,:]
-    r = 0
-    for y in range(bin_length):
-        id_list.append([num] * (temp1[y] - r))
-        id_samples.append((temp11[r:temp1[y]]))
-        r= temp1[y]
-        num = num + 1
-
-i = 1
-
-
-#### deep learning Algorithm
-
-id_list_flatten = list(itertools.chain(*id_list))
-id_samples_flatten = list(itertools.chain(*id_samples))
-
-print(len(id_list_flatten))
-print(len(id_list_flatten))
+    return raw_scg_value,id_number,raw_value_start_index,raw_value_end_index,targets_start_index,targets_end_index
