9d9b9b0ed942002deaecbbdea3e7809d1c22ef5c
nezih <nezih-aksu@hotmail.com>
2021-01-04 22:56:42 +0300

    Data windowing

--- signal_preprocess.py
+++ signal_preprocess.py
@@ -359,3 +359,127 @@ print(test_features_norm.shape)
 
 
 # Data windowing for BiLSTM model
+class WindowGenerator():
+    def __init__(self, input_width, label_width, shift,
+                 train_df=train_df, val_df=val_df, test_df=test_df,
+                 label_columns=None):
+        # Store the raw data.
+        self.train_df = train_df
+        self.val_df = val_df
+        self.test_df = test_df
+
+        # Work out the label column indices.
+        self.label_columns = label_columns
+        if label_columns is not None:
+            self.label_columns_indices = {name: i for i, name in
+                                          enumerate(label_columns)}
+        self.column_indices = {name: i for i, name in
+                               enumerate(train_df.columns)}
+
+        # Work out the window parameters.
+        self.input_width = input_width
+        self.label_width = label_width
+        self.shift = shift
+
+        self.total_window_size = input_width + shift
+
+        self.input_slice = slice(0, input_width)
+        self.input_indices = np.arange(self.total_window_size)[self.input_slice]
+
+        self.label_start = self.total_window_size - self.label_width
+        self.labels_slice = slice(self.label_start, None)
+        self.label_indices = np.arange(self.total_window_size)[self.labels_slice]
+
+    def __repr__(self):
+        return '\n'.join([
+            f'Total window size: {self.total_window_size}',
+            f'Input indices: {self.input_indices}',
+            f'Label indices: {self.label_indices}',
+            f'Label column name(s): {self.label_columns}'])
+
+
+def split_window(self, features):
+    inputs = features[:, self.input_slice, :]
+    labels = features[:, self.labels_slice, :]
+    if self.label_columns is not None:
+        labels = tf.stack(
+            [labels[:, :, self.column_indices[name]] for name in self.label_columns],
+            axis=-1)
+
+    # Slicing doesn't preserve static shape information, so set the shapes
+    # manually. This way the `tf.data.Datasets` are easier to inspect.
+    inputs.set_shape([None, self.input_width, None])
+    labels.set_shape([None, self.label_width, None])
+
+    return inputs, labels
+
+
+WindowGenerator.split_window = split_window
+
+
+def make_dataset(self, data):
+    data = np.array(data, dtype=np.float32)
+    ds = tf.keras.preprocessing.timeseries_dataset_from_array(
+        data=data,
+        targets=None,
+        sequence_length=self.total_window_size,
+        sequence_stride=1,
+        shuffle=True,
+        batch_size=32, )
+
+    ds = ds.map(self.split_window)
+    return ds
+
+
+WindowGenerator.make_dataset = make_dataset
+
+
+@property
+def train(self):
+    return self.make_dataset(self.train_df)
+
+
+@property
+def val(self):
+    return self.make_dataset(self.val_df)
+
+
+@property
+def test(self):
+    return self.make_dataset(self.test_df)
+
+
+WindowGenerator.train = train
+WindowGenerator.val = val
+WindowGenerator.test = test
+
+OUT_STEPS_In = 4
+OUT_STEPS_label = 1
+shift = 4
+# Predicting at appropriate step.
+wide_window = WindowGenerator(
+    input_width=OUT_STEPS_In, label_width=OUT_STEPS_label, shift=shift,
+    label_columns=["S", "D", "H", "R"])
+
+print(wide_window)
+
+MAX_EPOCHS = 150
+
+
+def compile_and_fit(model, window, patience=2):
+    early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss',
+                                                      patience=patience,
+                                                      mode='min')
+
+    model.compile(loss=tf.losses.MeanSquaredError(),
+                  optimizer=tf.optimizers.Adam(),
+                  metrics=[tf.metrics.MeanAbsoluteError()])
+
+    history = model.fit(window.train, epochs=MAX_EPOCHS,
+                        validation_data=window.val,
+                        callbacks=[early_stopping])
+
+    model.summary()
+    predicted_values = model.predict(test_features_norm)
+
+    return history, predicted_values
